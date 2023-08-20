40ff4f9e4d97160a77e8db8d4443bdfc5a8dd4e2
Chris <73894631+cdepaul06@users.noreply.github.com>
2023-08-20 12:01:01 -0400

    navigation is done?

--- GamingTemplatev1/src/App.jsx
+++ GamingTemplatev1/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState, useEffect } from "react";
+import React from "react";
+
 import Navigation from "./components/Navigation/";
 
-const App = () => {
+const App = ({}) => {
   return (
     <>
       <div className='relative min-h-screen'>
-        <div className='absolute top-0 left-1/2 transform -translate-x-1/2 z-10 pt-10'>
-          <Navigation />
-        </div>
+        <Navigation />
       </div>
     </>
   );
--- GamingTemplatev1/src/components/Navigation/Navigation.jsx
+++ GamingTemplatev1/src/components/Navigation/Navigation.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Button, Popover, List, ListItem, Divider } from "@mui/material";
+import { AccountCircle, PersonAdd } from "@mui/icons-material";
 import { navLinks } from "../../constants";
 
-const Navigation = () => {
+const Navigation = ({}) => {
   const [anchorEl, setAnchorEl] = useState(null);
   const [currentSubNav, setCurrentSubNav] = useState(null);
 
@@ -19,79 +20,101 @@ const Navigation = () => {
   const open = Boolean(anchorEl);
 
   return (
-    <div className='h-16'>
-      <div className='flex flex-col items-center justify-center w-full h-full border-t border-b bg-red-900'>
-        <ul className='flex items-center justify-center w-full h-full'>
-          {navLinks.map((link) => (
-            <li key={link.id} className='px-4 py-2'>
-              {link.subNavLinks ? (
-                <Button
-                  onClick={(e) => handlePopoverOpen(e, link.subNavLinks)}
-                  style={{
-                    backgroundColor: "rgb(145, 37, 37)",
-                    color: "white",
-                    boxShadow: "1px 3px 3px 1px rgba(0, 0, 0, 0.2)",
-                  }}
-                  size='large'
-                >
-                  {link.name}
-                </Button>
-              ) : (
-                <Button
-                  href={link.path}
-                  style={{
-                    backgroundColor: "rgb(145, 37, 37)",
-                    color: "white",
-                    boxShadow: "1px 3px 3px 1px rgba(0, 0, 0, 0.2)",
-                  }}
-                  size='large'
-                >
-                  {link.name}
-                </Button>
-              )}
-
-              {link.subNavLinks && (
-                <Popover
-                  open={open}
-                  anchorEl={anchorEl}
-                  onClose={handlePopoverClose}
-                  anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "center",
-                  }}
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "left",
-                  }}
-                  slotProps={{
-                    paper: {
-                      style: { backgroundColor: "rgb(145, 37, 37)" },
-                    },
-                  }}
-                >
-                  <List component='nav'>
-                    {currentSubNav &&
-                      currentSubNav.map((subLink, subIndex) => (
-                        <React.Fragment key={subLink.id}>
-                          <ListItem
-                            ListItemButton
-                            component='a'
-                            href={subLink.path}
-                            className='text-white'
-                          >
-                            {subLink.name}
-                          </ListItem>
-                          {subIndex !== currentSubNav.length - 1 && <Divider />}
-                        </React.Fragment>
-                      ))}
-                  </List>
-                </Popover>
-              )}
-            </li>
-          ))}
-        </ul>
+    <>
+      <div className='flex flex-1 justify-end'>
+        <Button
+          href='/login'
+          style={{
+            color: "white",
+          }}
+          size='large'
+        >
+          <AccountCircle className='mr-1' />
+          Login
+        </Button>
+        <Button
+          href='/signup'
+          style={{
+            color: "white",
+          }}
+          size='large'
+        >
+          <PersonAdd className='mr-1' />
+          Sign Up
+        </Button>
       </div>
-    </div>
+      <div className='h-16'>
+        <div className='flex flex-col items-center justify-center w-full h-full bg-red-900'>
+          <ul className='flex items-center justify-center w-full h-full'>
+            {navLinks.map((link) => (
+              <li key={link.id} className='px-4 py-2 hover:bg-red-700'>
+                {link.subNavLinks ? (
+                  <Button
+                    onClick={(e) => handlePopoverOpen(e, link.subNavLinks)}
+                    style={{
+                      color: "white",
+                    }}
+                    size='large'
+                  >
+                    {link.name}
+                  </Button>
+                ) : (
+                  <Button
+                    href={link.path}
+                    style={{
+                      color: "white",
+                    }}
+                    size='large'
+                  >
+                    {link.name}
+                  </Button>
+                )}
+
+                {link.subNavLinks && (
+                  <Popover
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={handlePopoverClose}
+                    anchorOrigin={{
+                      vertical: "bottom",
+                      horizontal: "center",
+                    }}
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "left",
+                    }}
+                    slotProps={{
+                      paper: {
+                        style: { backgroundColor: "rgb(145, 37, 37)" },
+                      },
+                    }}
+                  >
+                    <List component='nav'>
+                      {currentSubNav &&
+                        currentSubNav.map((subLink, subIndex) => (
+                          <React.Fragment key={subLink.id}>
+                            <ListItem
+                              ListItemButton
+                              component='a'
+                              href={subLink.path}
+                              className='text-white'
+                            >
+                              {subLink.name}
+                            </ListItem>
+                            {subIndex !== currentSubNav.length - 1 && (
+                              <Divider />
+                            )}
+                          </React.Fragment>
+                        ))}
+                    </List>
+                  </Popover>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
